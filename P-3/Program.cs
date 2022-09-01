c69f9115384065c67d9aad355dbe6819bdba41e8
Alex Karnakov <ak2005@yandex.ru>
2022-09-01 15:50:29 +0300

    Enter N -> build range '-N...0...N'

---
+++ P-3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести все целые числа от -N до N, которое ввел пользователь
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+
+Console.Write("Enter number: ");
+int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+int startNumber = number * -1;
+while (startNumber <= number)
+    {
+        Console.WriteLine(startNumber);
+        startNumber++;
+    }
