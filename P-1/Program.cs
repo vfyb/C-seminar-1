30e26baf80472a78f2514c9c0066aceb0f57d30b
Alex Karnakov <ak2005@yandex.ru>
2022-09-01 15:05:25 +0300

    Квадрат числв

---
+++ P-1/Program.cs
@@ -0,0 +1,8 @@
+﻿// User enter number
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+// 2 degree of number
+int result = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.WriteLine($"2 degree of number {number} = {result}");
