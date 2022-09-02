9de1ffdc10c3d1f34c236a5371d43f1f8dd73403
Alex Karnakov <ak2005@yandex.ru>
2022-09-02 17:12:33 +0300

    Zadanie-4 created

---
+++ Zadanie-4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+int even = 2;
+
+if (number == 1)
+    {
+        Console.WriteLine("There is no even numbers.");
+    }
+else
+    {
+        Console.WriteLine($"Even numders between 1 and {number}: ");
+        while ( even <= number )
+            {
+                Console.Write($"{even}, ");
+                even = even + 2;
+            }
+    }
