248e872c7b9003addef6251ab60721455b224a02
Alex Karnakov <ak2005@yandex.ru>
2022-09-02 16:57:49 +0300

    Zadanie-3 created

---
+++ Zadanie-3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+    {
+        Console.WriteLine($"Numder {number} is even");
+    }
+else 
+    {
+        Console.WriteLine($"Number {number} is odd");
+    }
