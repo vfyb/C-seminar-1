f00382ff74da391407336bb854e4582e9372bdeb
Alex Karnakov <ak2005@yandex.ru>
2022-09-05 17:00:48 +0300

    Delet second digit from number

---
+++ 2-Sem-2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int[] NumberToArray(int arg)
+{
+    string str_num = arg.ToString();
+    int[] num_array = new int[str_num.Length];
+    int i = 0;
+    while ( i < str_num.Length )
+    {
+        num_array[i] = int.Parse(str_num(i));
+        i++
+    }
+    return num_array;
+}
+
+int number = new Random().Next(100, 999);
+
+Console.WriteLine(num_array);
