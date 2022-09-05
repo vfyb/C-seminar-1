5dbfb4f280d5e1eb0ea56a40a200a8caa0bb2003
Alex Karnakov <ak2005@yandex.ru>
2022-09-05 15:15:07 +0300

    2 degree of number

---
+++ 1-2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вводим 2 числа и проверяем, является ли первое квадратом второго
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 == Convert.ToInt32(Math.Pow(number2, 2)))
+    {
+        Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+    }
+else    
+    {
+        Console.WriteLine($"Число {number1} НЕ является квадратом числа {number2}");
+    }
