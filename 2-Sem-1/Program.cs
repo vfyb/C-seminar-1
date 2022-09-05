adf74cf3d37e712f7e0dce56d0deb0aa31805133
Alex Karnakov <ak2005@yandex.ru>
2022-09-05 16:33:00 +0300

    'Max of 2 digit in two-digit number' function added

--- 2-Sem-1/Program.cs
+++ 2-Sem-1/Program.cs
@@ -1,18 +1,22 @@
 ﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] 
 // и показывает наибольшую цифру числа.
 
-int number = new Random().Next(10, 99);
+int MaxDigit(int arg)
+{
+    int second_digit = arg % 10;
+    int first_digit = (arg / 10) % 10;
 
+    int result = first_digit;
+
+if ( second_digit > result )
+    {
+        result = second_digit;
+    }
+    return result;
+}
+
+int number = new Random().Next(10, 99);
 Console.WriteLine(number);
 
-int second_digit = number % 10;
-int first_digit = (number / 10) % 10;
-
-int max = first_digit;
-
-if ( second_digit > max )
-    {
-        max = second_digit;
-    }
-
+int max = MaxDigit(number);
 Console.WriteLine(max);
