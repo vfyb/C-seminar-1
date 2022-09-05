4c87af06c4c300e0b48922853a49464c06e556e7
Alex Karnakov <ak2005@yandex.ru>
2022-09-05 15:18:35 +0300

    Delete files

--- 1-1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// User enter number
-Console.Write("Enter number: ");
-int number = int.Parse(Console.ReadLine());
-
-// 2 degree of number
-int result = Convert.ToInt32(Math.Pow(number, 2));
-
-Console.WriteLine($"2 degree of number {number} = {result}");
