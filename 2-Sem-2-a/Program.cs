e705267c4dd85a724f82c34589cb5c1de28198fa
Alex Karnakov <ak2005@yandex.ru>
2022-09-06 14:35:07 +0300

    Dlete second digit in number added

---
+++ 2-Sem-2-a/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа.
+
+void DeleteSecondDigit()
+    {
+        int number = new Random().Next(100, 1000);
+        int digit1 = number / 100;
+        int digit3 = number % 10;
+
+        int new_number = digit1 * 10 + digit3;
+
+        Console.WriteLine($"Generated number {number}");
+        Console.WriteLine($"Number without second digit {new_number}");
+    }
+
+DeleteSecondDigit();
