5f91489a7b629eddb6d40b6410b8043928d37396
Alex Karnakov <ak2005@yandex.ru>
2022-09-06 17:24:55 +0300

    Weekend or working day created

---
+++ 2-Zad-3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void WeekEnd(int num)
+    {
+        if ( num < 1 ^ num > 7) Console.WriteLine("You entered incorrect number (correct is from 1 to 7)");
+        else
+            {
+                if ( num == 6 ^ num == 7 ) Console.WriteLine($"Day {num} is weekend day.");
+                else Console.WriteLine($"Day {num} is working day.");
+            }
+    }
+Console.WriteLine("Enter number from 1 to 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+WeekEnd(number);
