71b3691fce5b3acd7dda921abfa224ff7a63ffb6
Alex Karnakov <ak2005@yandex.ru>
2022-09-06 21:06:52 +0300

    GetPointArea(int x, int y)

---
+++ 3-Sem-2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 
+// и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int GetPointArea(int x, int y)
+    {
+        if ( x > 0 && y > 0 ) return 1; 
+        if ( x < 0 && y > 0 ) return 2; 
+        if ( x < 0 && y < 0 ) return 3; 
+        if ( x > 0 && y < 0 ) return 4; 
+        return 0;
+    }
+
+Console.WriteLine("Enter X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int areaNum = GetPointArea(x, y);
+Console.WriteLine($"Area = {areaNum}");
