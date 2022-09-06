f4d46dc18426b8ef319491664b9d4f3e641d7258
Alex Karnakov <ak2005@yandex.ru>
2022-09-06 17:43:59 +0300

    Delete any digit in user number add comments

--- 2-Sem_Zad-2/Program.cs
+++ 2-Sem_Zad-2/Program.cs
@@ -1,5 +1,7 @@
-﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+﻿// Напишите программу, которая удаляет заданную цифру заданного числа 
+// или сообщает, что такой цифры нет..
 
+// Преобразует число в массив и возвращает этот массив:
 int[] NumberToArray(int arg)
 {
     string str_num = arg.ToString();
@@ -14,6 +16,7 @@ int[] NumberToArray(int arg)
     return num_array;
 }
 
+// Создает число в котором нет заданной цифры и возвращает новое число:
 int DeleteAnyDigit(int[] num_array, int dig_del)
 {
     string new_number = "";
@@ -34,15 +37,23 @@ int DeleteAnyDigit(int[] num_array, int dig_del)
     return result = int.Parse(new_number);
 }
 
-Console.WriteLine("Enter number: ");
+Console.WriteLine("Enter your number: ");
 int number = int.Parse(Console.ReadLine());
-// int number = new Random().Next(100, 9999);
 
 Console.WriteLine("Enter number of digit you want to delete: ");
 int digit_del = int.Parse(Console.ReadLine());
 
-int[] array = NumberToArray(number);
-int final_number = DeleteAnyDigit(array, digit_del);
+int final_number = number;
+
+// Узнаем отрицательное число или нет:
+int number_sign = 1;
+if ( number < 0 ) number_sign = -1;
+
+int[] array = NumberToArray(Math.Abs(number));
+
+// Узнаем есть ли в числе цифра с заданным номером:
+if ( array.Length < digit_del) Console.WriteLine($"There is no {digit_del} digit.");
+else final_number = number_sign * DeleteAnyDigit(array, digit_del);
 
 Console.WriteLine($"Your number is {number}.");
 Console.WriteLine($"Your number without {digit_del} digit is {final_number}.");
