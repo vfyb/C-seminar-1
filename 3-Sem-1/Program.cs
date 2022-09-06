0425249dbdfc784e8f9a0ccd644ef67350fd5a81
Alex Karnakov <ak2005@yandex.ru>
2022-09-06 21:05:44 +0300

    For and While created

---
+++ 3-Sem-1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] nums = new int[10] {10, 20, 45, 23, 76, 45, 3, 56, 78, 90};
+
+// int i = 0;
+int max = nums[0];
+
+for ( int i = 0; i < nums.Length; i++ )
+    {
+        if ( nums[i] > max )
+            {
+                max = nums[i];
+            }
+    }
+
+// while ( i < nums.Length )
+//     {
+//         if ( nums[i] > max )
+//             {
+//                 max = nums[i];
+//                 i++;
+//             }
+//         else i++;
+//     }
+
+Console.WriteLine(max);
