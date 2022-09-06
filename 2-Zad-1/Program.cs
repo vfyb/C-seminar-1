3ec81236e8b0d8fda89aecfbaee2984bf647210d
Alex Karnakov <ak2005@yandex.ru>
2022-09-06 15:37:45 +0300

    Second digit of three-digit number created

---
+++ 2-Zad-1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+void SecondDigit(int num)
+    {
+        if ( Math.Abs(num) < 100 ^ Math.Abs(num) > 999 ) 
+            {
+                Console.WriteLine("You entered NOT three-digit number.");
+            }
+        else 
+            {
+                int second_digit = Math.Abs(( num / 10 ) % 10);
+                Console.WriteLine($"Second digit of number {num} is:");
+                Console.WriteLine($"{second_digit}");
+            }
+        
+    }
+Console.WriteLine("Enter three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+SecondDigit(number);
