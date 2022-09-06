7f9d7326abbf83b5de4bbb44da4976113d43fbff
Alex Karnakov <ak2005@yandex.ru>
2022-09-06 16:58:53 +0300

    Print third digit of user number

---
+++ 2-Zad-2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+void ThirdDigitOfNumber(int num)
+    {
+        int rank = 2;
+
+        if ( Math.Abs(num) < 100 ) Console.WriteLine($"There is no third digit in number {num}");
+        else
+            {
+                // Determine the bit depth of the number (rank):
+                while ( Math.Pow(10, rank) <= Math.Abs(num))
+                    {
+                        rank++;
+                    }
+                Console.WriteLine($"Rank is {rank}");
+                rank = rank - 3;
+                int third_digit = (int)( Math.Abs(num) / Math.Pow(10, rank)) % 10 ;
+                
+                Console.WriteLine($"Third digit is {third_digit}");
+            }
+        
+    }
+Console.WriteLine("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ThirdDigitOfNumber(number);
