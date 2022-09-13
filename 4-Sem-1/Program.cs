24870ec3bc81cad254638e502942af08789ea470
Alex Karnakov <ak2005@yandex.ru>
2022-09-13 13:57:56 +0300

    Init commit

---
+++ 4-Sem-1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 24: 
+// Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+
+int SumNumber(int num)
+{
+    int result = 0;
+
+    for ( int i = 1; i <= num; i++)
+    {
+        result = result + i;
+    }
+
+    return result;
+}
+
+Console.WriteLine("Enter number: ");
+
+try
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int sign = 1;
+    if (number < 0) sign = -1;
+
+    int sum = SumNumber(Math.Abs(number)) * sign;
+
+    Console.WriteLine($"Sum of numbers from {sign} to {number}:");
+    Console.WriteLine($"{sum}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Not number was entered");
+}
+
+
---
+++ 4-Sem-2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 26: 
+// Напишите программу, которая принимает на вход число и 
+// выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// int NumberOfDigits(int number)
+// {
+//     int result = (Convert.ToString(number)).Length;
+//     return result; 
+// }
+
+int NumberOfDigits(int number)
+{
+    int result = 0;
+    if ( number == 0 ) result = 1;
+    else
+    {
+        while ( number != 0 )
+        {
+            number = number / 10;
+            result++;
+        }
+    }
+    return result; 
+}
+
+Console.WriteLine("Enter number: ");
+
+try
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    int num_digits = NumberOfDigits(Math.Abs(number));
+
+    Console.WriteLine($"Number of digits in {number} is {num_digits}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Not number was entered");
+}
