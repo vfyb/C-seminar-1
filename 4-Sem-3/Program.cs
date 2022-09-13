95f15e919ef3cfa5c4c2705192b92cd551f4b4f2
Alex Karnakov <ak2005@yandex.ru>
2022-09-13 14:23:05 +0300

    Create array with 8 elements, filled with 0 and 1 randomly

---
+++ 4-Sem-3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Create array with 8 elements, filled with 0 and 1 randomly
+
+int[] RandomArray()
+{
+    int[] array = new int[8];
+
+    for ( int i = 1; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void PrintArrayToConsole(int[] array)
+{
+    for ( int i = 1; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+
+int[] array = RandomArray();
+PrintArrayToConsole(array);
+Console.WriteLine("Hello, World!");
