1a9d09e53c3c482dcc8f5f91dcb061b984822d7a
Alex Karnakov <ak2005@yandex.ru>
2022-09-14 17:35:38 +0300

    Quantity In Diapazone in array

---
+++ 5-Sem-4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 35: 
+// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+int[] CreateRandomArray()
+{
+    int size = 123;
+    int[] array = new int[size];
+    var random = new Random();
+
+    for ( int i = 0; i < size; i++ ) array[i] = random.Next(-300, 300);
+    
+    return array;
+}
+
+int GetQuantityInDiapazone(int[] array)
+{
+    int result = 0;
+
+    for ( int i = 0; i < array.Length; i++ )
+    {
+        if ( array[i] >= 10 && array[i] <= 99 ) result++;
+    }  
+
+    return result;  
+}
+
+int[] newArray = CreateRandomArray();
+for ( int i = 0; i < newArray.Length; i++ ) Console.Write($"{newArray[i]}, ");
+
+int quantityInDiapazone = GetQuantityInDiapazone(newArray);
+Console.WriteLine("");
+Console.WriteLine($"Quantity of element in diapazone [10 ,99]: {quantityInDiapazone}");
