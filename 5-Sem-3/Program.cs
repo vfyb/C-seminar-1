f5c5ca10c3aa8996f3125c9a9bb453d00a4647ce
Alex Karnakov <ak2005@yandex.ru>
2022-09-14 16:04:12 +0300

    Check number in or not in array

---
+++ 5-Sem-3/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 33: 
+// Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+//  4; массив [6, 7, 19, 345, 3] -> нет 
+//  -3; массив [6, 7, 19, 345, -3] -> да
+
+int[] CreateNewRandomArray()
+{
+    int size = 12;
+    int[] array = new int[size];
+
+    for ( int i = 0; i < size; i++ ) array[i] = new Random().Next(-100, 100);
+    
+    return array;
+}
+
+bool CheckElementInArray(int[] array, int number)
+{
+    bool result = false;
+    
+    for ( int i = 0; i < array.Length; i++ ) 
+    {
+        if ( array[i] == number ) 
+        {
+            result = true;
+            break;
+        }
+    }
+    
+    return result;
+    
+}
+
+int[] newArray = CreateNewRandomArray();
+for ( int i = 0; i < newArray.Length; i++ ) Console.Write($"{newArray[i]}, ");
+
+Console.WriteLine(" ");
+Console.WriteLine("Enter number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool check_element = CheckElementInArray(newArray, number);
+
+if ( check_element == true ) Console.WriteLine($"Number {number} is in array.");
+else Console.WriteLine($"Number {number} is not in array.");
